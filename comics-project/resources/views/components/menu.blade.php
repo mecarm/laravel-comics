693ae4e14abfba0ccd1adfa1a17254ea3760a99e
mecarm <melecarmine99@gmail.com>
2023-01-12 16:40:06 +0100

    header comp

---
+++ comics-project/resources/views/components/menu.blade.php
@@ -0,0 +1,20 @@
+<header>
+
+    <img src="{{ asset('img/dc-logo.png') }}" alt="DC logo">
+    
+    <div> 
+      <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </div>
+
+  </header>
