57c14a9ddccd0b929a5a359b5b3c9377c24a866d
mecarm <melecarmine99@gmail.com>
2023-01-13 15:57:38 +0100

    Creazione card fumetti singoli dinamiche

--- comics-project/resources/views/prodotto.blade.php
+++ comics-project/resources/views/prodotto.blade.php
@@ -3,5 +3,13 @@
 @section('page-title', "DC Comics - Fumetto")
 
 @section('main-content')
-    <h2>Prodotto: {{ $prodotto_singolo['title'] }}</h2>
+
+    <div class="card-comic">
+        <img src="{{ $prodotto_singolo['thumb']}}" alt="Comic Image" class="cards-img-top">
+        <div class="cards-body">
+          <h5 class="cards-title">{{ $prodotto_singolo['title'] }}</h5>
+          <p class="cards-text">{{ $prodotto_singolo['description'] }}</p>
+        </div>
+      </div>
+
 @endsection
