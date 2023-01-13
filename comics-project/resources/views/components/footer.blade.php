9daf11f91c86b4b318af62eb60c191bc248482d1
mecarm <melecarmine99@gmail.com>
2023-01-13 22:24:08 +0100

    agg footer

--- comics-project/resources/views/components/footer.blade.php
+++ comics-project/resources/views/components/footer.blade.php
@@ -20,3 +20,75 @@
         <div>DC POWER VISA</div>
     </div>
   </div>
+    <div class="footer">
+        <div class="footer-bg">
+            <div class="list">
+                <div id="list">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <ul>
+                    <h3>DC COMICS</h3>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div id="list">
+                <ul>
+                    <h3>DC</h3>
+                    <li>Term Of Use</li>
+                    <li>Privacy Policy(New)</li>
+                    <li>Ad Choises</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subiscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div id="list">
+                <ul>
+                    <h3>SITES</h3>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <div class="dc-bg">
+            <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
+        </div>
+    
+    </div>
+    <div class="social">
+        <div>
+            <div>
+                <button>SIGN UP NOW!</button>
+            </div>
+            <div class="follow">
+                <div>
+                    <strong>FOLLOW US</strong>
+                </div>
+                <div class="icon">
+                    <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+                    <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+                    <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+                    <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+                    <img src="{{ asset('img/footer-periscope.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
