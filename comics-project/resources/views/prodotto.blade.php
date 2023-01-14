a998ebb54d88a087c46b5d261457a448d408189c
mecarm <melecarmine99@gmail.com>
2023-01-14 16:37:40 +0100

    Style card fumetto

--- comics-project/resources/views/prodotto.blade.php
+++ comics-project/resources/views/prodotto.blade.php
@@ -4,10 +4,23 @@
 
 @section('main-content')
 
+    <div class="blu-bar">
+      <img src="{{ $prodotto_singolo['thumb']}}" alt="Comic Image" class="cards-img-top">
+    </div>
+
     <div class="card-comic">
-        <img src="{{ $prodotto_singolo['thumb']}}" alt="Comic Image" class="cards-img-top">
         <div class="cards-body">
           <h5 class="cards-title">{{ $prodotto_singolo['title'] }}</h5>
+          <div class="green-bar">
+            <div class="price">
+              <span>U.S. Price: {{ $prodotto_singolo['price'] }}</span>
+              <span>Availabile</span>
+            </div>
+            <div class="availability">
+                <div class="line"></div>
+                <div class="check">Check Availability</div>
+            </div>
+          </div>        
           <p class="cards-text">{{ $prodotto_singolo['description'] }}</p>
         </div>
       </div>
